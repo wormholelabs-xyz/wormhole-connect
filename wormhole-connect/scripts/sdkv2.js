737b9bf8a0475cd3d9724c7526da9b919119e0a8
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-06-26 18:57:28 -0400

    change sdk install method

--- wormhole-connect/scripts/sdkv2.js
+++ wormhole-connect/scripts/sdkv2.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
 
 const WORK_ROOT = process.env['WORK_ROOT'];
-
 if (!WORK_ROOT) {
   console.error(
     'Please export a WORK_ROOT env var containing the absolute path to a directory containing the following repos:\n- wormhole-sdk-ts\n- example-native-token-transfers',
@@ -10,65 +10,28 @@ if (!WORK_ROOT) {
   process.exit(1);
 }
 
-const SDK_PATH = path.join(WORK_ROOT, 'wormhole-sdk-ts');
-const NTT_SDK_PATH = path.join(
-  WORK_ROOT,
-  'example-native-token-transfers/sdk/definitions',
-);
+const sdkPath = path.join(WORK_ROOT, 'wormhole-sdk-ts');
+const sdkPackageJsonPath = path.join(sdkPath, './package.json');
 
-if (!SDK_PATH) throw new Error('Please set SDK_PATH in your environment');
-
-const { execSync } = require('child_process');
-
-// This script builds, packs, and installs sdkv2 from a local directory
-
-const sdkPackageJsonPath = path.join(SDK_PATH, './package.json');
-
-// Get SDKv2 version
 const { version, workspaces } = JSON.parse(
   fs.readFileSync(sdkPackageJsonPath, 'utf8'),
 );
 
-let sdkPackages = {};
+// prepare packages
+execSync(`npm pack --workspaces`, { cwd: sdkPath });
 
+// get list of packages
+let sdkPackages = {};
 for (const workspace of workspaces) {
   if (workspace.includes('examples')) continue;
-  const workspacePackageJson = path.join(SDK_PATH, workspace, 'package.json');
+
+  const workspacePackageJson = path.join(sdkPath, workspace, 'package.json');
+
   const { name } = JSON.parse(fs.readFileSync(workspacePackageJson));
-  sdkPackages[name] = path.join(SDK_PATH, workspace);
+  const tgz = name.replace('@', '').replace('/', '-') + '-' + version + '.tgz';
 
-  console.log(`linking ${name}`);
-  execSync('npm link', { cwd: sdkPackages[name] });
+  sdkPackages[name] = path.join(sdkPath, tgz);
 }
 
-//sdkPackages['@wormhole-foundation/sdk-definitions-ntt'] = NTT_SDK_PATH;
-
-execSync(`npm link ${Object.keys(sdkPackages).join(' ')}`, {
-  cwd: path.join(__dirname, '../'),
-});
-
-for (const name in sdkPackages) {
-  if (name.includes('examples')) {
-    continue;
-  }
-  linkLocalSdkPackages(sdkPackages[name]);
-}
-
-function linkLocalSdkPackages(dir) {
-  let packageJson = JSON.parse(fs.readFileSync(path.join(dir, 'package.json')));
-  packageJson.overrides = packageJson.overrides || {};
-
-  let keys = [];
-
-  for (let key in packageJson.dependencies) {
-    if (sdkPackages[key] !== undefined) {
-      keys.push(key);
-    }
-  }
-
-  console.log(`npm link ${keys.join(' ')}`);
-
-  execSync(`npm link ${keys.join(' ')}`, { cwd: dir });
-}
-
-execSync(`rm -rf wormhole-connect/node_modules/@wormhole-foundation`);
+// install packages
+execSync(`npm install ${Object.values(sdkPackages).join(' ')}`);
