18ec3f0b3ffc3fa609411d27b4e2897c4a3d625d
Artur Sapek <art@wormholelabs.xyz>
2025-01-29 16:04:49 -0500

    various tiny little things

--- wormhole-connect/src/components/DemoApp/index.tsx
+++ wormhole-connect/src/components/DemoApp/index.tsx
@@ -356,47 +356,42 @@ function DemoApp() {
                   Available theme properties:
                   <ul className="available-properties">
                     <li>
-                      {' '}
                       <pre>mode</pre>
                       <i>'dark' | 'light'</i>
                     </li>
                     <li>
-                      {' '}
+                      <pre>background</pre>
+                      <i>string;</i>
+                    </li>
+                    <li>
                       <pre>input</pre>
                       <i>string;</i>
                     </li>
                     <li>
-                      {' '}
                       <pre>primary</pre>
                       <i>string;</i>
                     </li>
                     <li>
-                      {' '}
                       <pre>secondary</pre>
                       <i>string;</i>
                     </li>
                     <li>
-                      {' '}
                       <pre>text</pre>
                       <i>string;</i>
                     </li>
                     <li>
-                      {' '}
                       <pre>textSecondary</pre>
                       <i>string;</i>
                     </li>
                     <li>
-                      {' '}
                       <pre>error</pre>
                       <i>string;</i>
                     </li>
                     <li>
-                      {' '}
                       <pre>success</pre>
                       <i>string;</i>
                     </li>
                     <li>
-                      {' '}
                       <pre>font</pre>
                       <i>string;</i>
                     </li>
--- wormhole-connect/src/views/v2/Bridge/index.tsx
+++ wormhole-connect/src/views/v2/Bridge/index.tsx
@@ -449,7 +449,7 @@ const Bridge = () => {
       variant="primary"
       className={classes.reviewTransaction}
       disabled={reviewTransactionDisabled}
-      onClick={() => {
+      onMouseDown={() => {
         dispatch(setTransferRoute(selectedRoute));
         setWillReviewTransaction(true);
       }}
